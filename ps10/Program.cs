5222e92653c5bf3dc3f9828e4cb845c17d210ef8
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 18:32:37 +0530

    Add project files.

---
+++ ps10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInput first integer ");
+            int x = Convert.ToInt32(Console.ReadLine())
+;            Console.WriteLine("\nInput second integer");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("check if one is negative and one is positive : ");
+            Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+            Console.ReadLine();
+        }
+    }
+}
